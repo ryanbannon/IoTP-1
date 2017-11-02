0601730236d5f47626b28c93d7673133b7d68078
Ryan Bannon <ryansbannon@gmail.com>
2017-11-02 12:42:00 +0000

    working graphs retrieving test data from dweet

---
+++ dweet.py
@@ -0,0 +1,38 @@
+# import fcntl, socket, struct,
+import dweepy, time, platform, random
+
+def getTemp():
+    return random.randint(1,1000)
+    # Utilize and adjust sample code given by Grove to your needs
+
+def getHumidity():
+    return random.randint(1,1000)
+    # Utilize and adjust sample code given by Grove to your needs
+
+def getNoiseLevel():
+    return random.randint(1,1000)
+    # Utilize and adjust sample code given by Grove to your needs
+
+# from http://stackoverflow.com/questions/159137/getting-mac-address
+'''
+def getHwAddr(ifname):
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    info = fcntl.ioctl(s.fileno(), 0x8927,  struct.pack('256s', ifname[:15]))
+    return ':'.join(['%02x' % ord(char) for char in info[18:24]])
+'''
+
+def post(dic):
+    thing = 'ryan-iot-ca1'
+    print dweepy.dweet_for(thing, dic)
+
+def getReadings():
+    dict = {}
+    dict["temperature"] = getTemp()
+    dict["humidity"] = getHumidity()
+    dict["noise_level"] = getNoiseLevel()
+    return dict
+
+while True:
+    dict = getReadings();
+    post(dict)
+    time.sleep(5)
