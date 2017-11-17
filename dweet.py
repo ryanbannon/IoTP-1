3275379f9a7135b084f2b8293b22e1899b6605d5
Ryan Bannon <ryansbannon@gmail.com>
2017-11-17 16:15:13 +0000

    final completed commit

--- dweet.py
+++ dweet.py
@@ -1,12 +1,11 @@
 import dweepy
-import random
 import config
 import grovepi
 from grovepi import *
 from time import sleep
+import sqlite3
 
 def getDistance():
-
     ultrasonic_ranger = 4
 
     while True:
@@ -18,8 +17,20 @@ def getDistance():
         except IOError:
             print ("Error")
 
-def getTemperature():
+def getLight():
+    light_sensor = 1
 
+    pinMode(light_sensor,"INPUT")
+
+    while True:
+        try:
+            sensor_value = analogRead(light_sensor)
+            return sensor_value
+
+        except IOError:
+            print ("Error")
+
+def getTemperature():
     dht_sensor = 3
 
     while True:
@@ -32,36 +43,26 @@ def getTemperature():
         except IOError:
             print ("Error")
 
-def getLight():
-
-    #Does the light sensor need to be changed to port A1 so light_sensor value [0] does
-    #not clash with dht blue sensor also [0]
-    light_sensor = 0
-
-    grovepi.pinMode(light_sensor,"INPUT")
-
-    while True:
-        try:
-            sensor_value = grovepi.analogRead(light_sensor)
-            return sensor_value
-            sleep(1)
-
-        except IOError:
-            print ("Error")
-
 def post(dic):
-
     thing = config.thing
     print dweepy.dweet_for(thing, dic)
 
 def getReadings():
     dict = {}
     dict["distance"] = getDistance()
-    dict["temperature"] = getTemperature()
     dict["light_level"] = getLight()
+    dict["temperature"] = getTemperature()
     return dict
 
 while True:
     dict = getReadings();
     post(dict)
+
+    conn = sqlite3.connect("dweet.sqlite")
+    #c = conn.cursor()
+    #c.execute('IF NOT EXISTS (CREATE TABLE Sensor_Readings(dist_value INTEGER, light_value INTEGER, temp_value INTEGER));')
+    #dbUpdate = "INSERT INTO Sensor_Readings(dist_value, light_value, temp_value) VALUES(dict["distance"], dict["light_level"], dict["temperature"])"
+    #conn.commit()
+    #c.execute(dbUpdate)
+
     sleep(5)
