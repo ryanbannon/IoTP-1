82525dc45f6012a171448810a483556639a4807b
Ryan Bannon <ryansbannon@gmail.com>
2017-11-17 00:11:16 +0000

    adding final pieces together

--- dweet.py
+++ dweet.py
@@ -1,14 +1,12 @@
 import dweepy
-#import platform
 import random
 import config
-#import grovepi
-#from grovepi import *
+import grovepi
+from grovepi import *
 from time import sleep
 
 def getDistance():
-    return random.randint(1,100)
-    '''
+
     ultrasonic_ranger = 4
 
     while True:
@@ -19,28 +17,25 @@ def getDistance():
 
         except IOError:
             print ("Error")
-    '''
-def getTemperature():
-    return random.randint(1,30)
-    '''
-    sensor = 3
 
-    blue = 0
-    white = 1
+def getTemperature():
+
+    dht_sensor = 3
 
     while True:
         try:
-            [temp] = grovepi.dht(sensor,blue)
-            if math.isnan(temp) == False:
-                return temp
-                sleep(1)
+            [temp,hum] = dht(dht_sensor,0)
+            temperature = str(temp)
+            return temperature
+            sleep(1)
 
         except IOError:
             print ("Error")
-    '''
+
 def getLight():
-    return random.randint(1,500)
-    '''
+
+    #Does the light sensor need to be changed to port A1 so light_sensor value [0] does
+    #not clash with dht blue sensor also [0]
     light_sensor = 0
 
     grovepi.pinMode(light_sensor,"INPUT")
@@ -53,10 +48,8 @@ def getLight():
 
         except IOError:
             print ("Error")
-    '''
+
 def post(dic):
-    #thing = 'ryan-iot-ca1'
-    #print dweepy.dweet_for(thing, dic)
 
     thing = config.thing
     print dweepy.dweet_for(thing, dic)
